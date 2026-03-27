7f05dfc6dc784e9882172310768731f18c032ad4
амина <temeshamina2007@gmail.com>
2026-03-27 11:45:33 +0500

    main2 python

---
+++ lec.py
@@ -0,0 +1,26 @@
+"""score = int(input("Enter your score: "))
+if score > 50:
+    print("You passed")
+else:
+    print("You failed")"""
+
+"""week = input("Enter a day of the week: ")
+if week == "Saturday" or week == "Sunday":
+    print("Weekend")
+else:
+    print("Weekday")"""
+
+
+"""i = 1
+while i <= 5:
+    print(i)
+    i += 1"""
+
+"""word = input("Введите слово: ")
+
+for letter in word:
+    print(letter)"""
+
+word = input("Enter a word: ")
+print (word.upper())
+print (word.lower())
